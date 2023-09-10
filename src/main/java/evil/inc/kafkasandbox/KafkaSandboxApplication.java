06094f3b497a16e1b45d2f37cc6dca477bd299c7
Ion Pascari <ionpascari1995@gmail.com>
2023-09-10 13:53:45 +0300

    feat: initial commit

---
+++ src/main/java/evil/inc/kafkasandbox/KafkaSandboxApplication.java
@@ -0,0 +1,13 @@
+package evil.inc.kafkasandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/evil/inc/kafkasandbox/KafkaSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package evil.inc.kafkasandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
