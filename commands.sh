e5160e31aa4e47d2d00f4c87ce76e83a707d2921
Ion Pascari <ionpascari1995@gmail.com>
2023-09-18 09:42:56 +0300

    feat: play around with kafka compaction log

--- commands.sh
+++ commands.sh
@@ -18,3 +18,29 @@ broker kafka-configs --bootstrap-server localhost:9092 --alter --add-config 'pro
 broker kafka-configs --bootstrap-server localhost:9092 --alter --add-config 'producer_byte_rate=1024,consumer_byte_rate=2048' --entity-name user1 --entity-type users
 
 broker kafka-configs --bootstrap-server localhost:9092 --alter --add-config 'consumer_byte_rate=2048' --entity-type users
+
+#compaction log
+broker kafka-topics --create --bootstrap-server localhost:9092  --topic employee-salary \
+    --partitions 1 --replication-factor 1 \
+    --config cleanup.policy=compact \
+    --config min.cleanable.dirty.ratio=0.001 \
+    --config segment.ms=5000
+
+broker kafka-topics --describe --bootstrap-server broker:9092 --topic employee-salary
+broker kafka-console-consumer --bootstrap-server localhost:9092 \
+    --topic employee-salary \
+    --from-beginning \
+    --property print.key=true \
+    --property key.separator=,
+broker kafka-console-producer --bootstrap-server localhost:9092 \
+    --topic employee-salary \
+    --property parse.key=true \
+    --property key.separator=,
+
+#To produce
+#Patrick,salary: 10000
+#Lucy,salary: 20000
+#Bob,salary: 20000
+#Patrick,salary: 25000
+#Lucy,salary: 30000
+#Patrick,salary: 30000
