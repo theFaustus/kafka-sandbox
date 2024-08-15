47b65ff00ab7cd4b9d9518d79e645c561c1e0896
Ion Pascari <ionpascari1995@gmail.com>
2024-08-15 22:40:00 +0300

    feat: play with more commands

--- commands.sh
+++ commands.sh
@@ -2,39 +2,73 @@ alias broker='docker exec -it broker'
 
 docker run --rm -it --net=host landoop/fast-data-dev bash
 
-broker kafka-topics --list --bootstrap-server broker:9092
+docker exec -it kafka-cluster bash
 
-broker kafka-topics --create --bootstrap-server broker:9092 --replication-factor 1 --partitions 1 --topic test-topic
+kafka-topics --list --bootstrap-server localhost:9092
 
-broker kafka-topics --bootstrap-server broker:9092 --topic test-topic --describe
+kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 2 --topic test-topic
 
-broker kafka-console-producer --bootstrap-server broker:9092 --topic test-topic
+kafka-topics --bootstrap-server localhost:9092 --topic test-topic --describe
 
-broker kafka-console-consumer --bootstrap-server broker:9092 --topic test-topic
+kafka-consumer-groups \
+--bootstrap-server localhost:9092 \
+--group ps \
+--topic test-topic \
+--reset-offsets \
+--shift-by -10 \
+--execute
+#--dry-run - for simulation
 
-broker kafka-topics --create --bootstrap-server broker:9092 --replication-factor 1 --partitions 5 --topic CustomersWithPartitionForMichaelScott
+kafka-consumer-groups \
+--bootstrap-server localhost:9092 \
+--group ps \
+--describe
+
+kafka-console-producer --bootstrap-server localhost:9092 --topic test-topic
+
+#key is needed for murmur2 algorithm to decide to which partition to send
+kafka-console-producer \
+--bootstrap-server localhost:9092 \
+--topic test-topic \
+--property key.separator=: \
+--property parse.key=true
+
+kafka-console-consumer --bootstrap-server localhost:9092 --topic test-topic
+
+kafka-console-consumer \
+--bootstrap-server localhost:9092 \
+--topic test-topic \
+--from-beginning \
+--property print.key=true \
+--property print.offset=true \
+--property print.timestamp=true  \
+--group ps
+
+kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 5 --topic CustomersWithPartitionForMichaelScott
 
 #set up quotas
-broker kafka-configs --bootstrap-server localhost:9092 --alter --add-config 'producer_byte_rate=1024' --entity-name clientC --entity-type clients
+kafka-configs --bootstrap-server localhost:9092 --alter --add-config 'producer_byte_rate=1024' --entity-name clientC --entity-type clients
 
-broker kafka-configs --bootstrap-server localhost:9092 --alter --add-config 'producer_byte_rate=1024,consumer_byte_rate=2048' --entity-name user1 --entity-type users
+kafka-configs --bootstrap-server localhost:9092 --alter --add-config 'producer_byte_rate=1024,consumer_byte_rate=2048' --entity-name user1 --entity-type users
 
-broker kafka-configs --bootstrap-server localhost:9092 --alter --add-config 'consumer_byte_rate=2048' --entity-type users
+kafka-configs --bootstrap-server localhost:9092 --alter --add-config 'consumer_byte_rate=2048' --entity-type users
 
 #compaction log
-broker kafka-topics --create --bootstrap-server localhost:9092  --topic employee-salary \
+kafka-topics --create --bootstrap-server localhost:9092  --topic employee-salary \
     --partitions 1 --replication-factor 1 \
     --config cleanup.policy=compact \
     --config min.cleanable.dirty.ratio=0.001 \
     --config segment.ms=5000
 
-broker kafka-topics --describe --bootstrap-server broker:9092 --topic employee-salary
-broker kafka-console-consumer --bootstrap-server localhost:9092 \
+kafka-topics --describe --bootstrap-server localhost:9092 --topic employee-salary
+
+kafka-console-consumer --bootstrap-server localhost:9092 \
     --topic employee-salary \
     --from-beginning \
     --property print.key=true \
     --property key.separator=,
-broker kafka-console-producer --bootstrap-server localhost:9092 \
+
+kafka-console-producer --bootstrap-server localhost:9092 \
     --topic employee-salary \
     --property parse.key=true \
     --property key.separator=,
--- src/main/java/evil/inc/kafkasandbox/kafkastreams/bankaccount/commands.sh
+++
@@ -1,7 +0,0 @@
-alias broker='docker exec -it kafka-cluster'
-
-docker run --rm -it --net=host landoop/fast-data-dev bash
-
-kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 2 --topic bank-account-input
-
-kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 2 --topic bank-account-output
